0994280b9f3285baa4c2c33d4e1e40e601044429
Erick Serejo <erickrainier@hotmail.com>
2025-02-20 17:08:00 -0300

    feat: add font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,14 @@ export default defineNuxtConfig({
   supabase: {
     redirect: false,
   },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
+        },
+      ],
+    },
+  },
 });
